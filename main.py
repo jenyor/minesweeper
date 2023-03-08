12155543cfc4da8be9b339a123f64a3348482680
EvMaMis <evmamis13@gmail.com>
2023-03-08 22:56:20 +0200

    Comments fix

--- main.py
+++ main.py
@@ -26,20 +26,18 @@ def main():
     )
 
     args = parser.parse_args()
-    """
-    Trying to check values of the config, to avoid too big amount of mines or negative screen size, fields or mines
-    """
+    # Trying to check values of the config, to avoid too big amount of mines or negative screen size, fields or mines
     try:
         game_config = config.Config(tuple(args.blocks), args.mines, tuple(args.screensize), args.difficulty)
     except Exception as err:
         print(f"\n\033[31m ERROR: {err.args[0]} \n\033[0m")
         sys.exit(1)
-    """If some Exception occured, it will tell user what's the problem in program and exit from the game"""
+    # If some Exception occured, it will tell user what's the problem in program and exit from the game
     start = True
-    """Returns true if Space key pressed. Space is used for restart game with the same configuration and board"""
+    # Returns true if Space key pressed. Space is used for restart game with the same configuration and board
     while start:
         start = game.Game(game_config).run()
-    """If Space key wasn't pressed, the game will start only once"""
+    # If Space key wasn't pressed, the game will start only once
 
 
 if __name__ == "__main__":
